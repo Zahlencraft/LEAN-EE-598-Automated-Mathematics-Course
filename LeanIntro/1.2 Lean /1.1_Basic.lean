729e79d21e7c286263c70b5a037ecb55397c71fd
Zahlencraft <acharya18261027@gmail.com>
2026-09-14 20:37:29 +0545

    cleared some errors. The types thingy seems intresting. want to know how it's working internally and how does it do it mathematically.

--- LeanIntro/1.2 Lean /1.1_Basic.lean
+++ LeanIntro/1.2 Lean /1.1_Basic.lean
@@ -10,20 +10,24 @@ example (x y z : ℚ)
   sorry
 
 --exercise
-theorem T₁ : ∀ x : ℝ, 0 ≤ x ^ 2 := by
-  intro x
-  simpa [pow_two] using sq_nonneg x
+theorem T₁ : ∀ x : ℝ, 0 ≤ x ^ 2 := by sorry
 
 -- Type Checking
 -- L∃∀N is based on type theory.
 -- This means that every term has a very well-defined type. To find the type of an expression, use #check. The result will show up in the Infoview.
 
-#check 1
-#check "1"
-#check ∃ (x : Nat), x < 0
-#check fun x => x+1
+#check 1  --ℕ
+#check "1" -- "1" : String
+#check ∃ (x : Nat), x < 0 -- ∃ (x : Nat), x < 0 : Prop
+#check fun x => x+1  -- fun x => x + 1 : Nat → Nat
 
 --Exercise
-#check (4, 5) --
-#check ℕ × ℕ
-#check Type
+#check (4, 5) -- (4, 5) : ℕ × ℕ
+
+#check ℕ × ℕ -- ℕ × ℕ : Type
+
+#check Type -- Type : Type 1
+
+-- check is doing something like not only checking the type of an expression, but also inferring the type of the expression.
+
+-- like why ℕ × ℕ  is Type, because ℕ is Type, and the product of two types is also a type.
